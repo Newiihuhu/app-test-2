e6cebb6591a757c4483aa62b9ef56da2abf8850a
Newiihuhu <Ninewii_huhu@hotmail.com>
2022-02-20 15:33:33 +0700

    build

--- app-test-2/src/App.js
+++ app-test-2/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi, I'm Newii.
+          Hi, I'm Newiihuhu.
         </p>
         <a
           className="App-link"
